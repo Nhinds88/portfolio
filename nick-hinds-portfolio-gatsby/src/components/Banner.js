777054c16d71d59b2c0768f576cea5838b67a882
Nhinds88 <nhinds@csumb.edu>
2021-08-01 21:24:24 -0700

    added some pics, need more

---
+++ nick-hinds-portfolio-gatsby/src/components/Banner.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Banner = (props) => (
+    <section id="banner" className="major">
+        <div className="inner">
+            <header className="major">
+                <h1>Hi, my name is Nick</h1>
+            </header>
+            <div className="content">
+                <p>I am a recent graduate at CSUMB's Computer Science program. I like building web and mobile projects, check out some of projects below!</p>
+                <ul className="actions">
+                    <li><a href="#one" className="button next scrolly">Projects</a></li>
+                </ul>
+            </div>
+        </div>
+    </section>
+)
+
+export default Banner
